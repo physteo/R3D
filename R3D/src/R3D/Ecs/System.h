3ef3f1f6975689ca4a9c16e9a1698bb63490371d
physteo <matteo_campo91@live.it>
2020-01-24 14:53:47 +0100

    Readded System.h that was mistakenly removed.

---
+++ R3D/src/R3D/Ecs/System.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <R3D/Ecs/ArchetypeManager.h>
+
+namespace r3d
+{
+
+	class System : public EventsListener
+	{
+
+	public:
+		System() {}
+
+		virtual void update(ArchetypeManager& am, double t, double dt) = 0;
+		virtual bool onEvent(Event& e) { return false; }
+		inline const std::list<EventType>& getEventsOfInterest() const { return m_interestingEvents; };
+
+	protected:
+		std::list<EventType> m_interestingEvents;
+
+		inline void addInterestToEvent(EventType et) { m_interestingEvents.push_back(et); }
+
+		template<class C>
+		size_t getSize(ArchetypeManager& am, size_t archId)
+		{
+			return am.m_archetypeDataVector[archId].get<C>()->size();
+		}
+
+		template<class C>
+		std::vector<Entity>& getEntities(ArchetypeManager& am, size_t archId)
+		{
+			return am.m_archetypeDataVector[archId].get<C>()->getEntities();
+		}
+
+		template<class C>
+		C* get(ArchetypeManager& am, size_t archId)
+		{
+//			auto archetypeData = am.m_archetypeDataVector[archId].get<C>();
+//			return archetypeData->getComponents< C > ( );
+			return nullptr;
+		}
+
+	};
+
+}
+
